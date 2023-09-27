8363379e1abd3e1cef7b43592407adf77a1d2e86
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-09-27 19:50:35 -0300

    Mejoras en el script de bsede datos.

--- ScriptsSQL/script_tabla_asteroides.sql
+++ ScriptsSQL/script_tabla_asteroides.sql
@@ -1,18 +1,18 @@
 CREATE TABLE [norman_ruiz_coderhouse].[asteroides](
 	[links] VARCHAR(MAX) NOT NULL,
-	[id] INTEGER NOT NULL,
+	[id] INTEGER NOT NULL PRIMARY KEY,
 	[neo_reference_id] INTEGER NOT NULL,
 	[name] VARCHAR(50) NOT NULL,
 	[nasa_jpl_url] VARCHAR(max) NOT NULL,
 	[absolute_magnitude_h] DECIMAL(8,2) NOT NULL,
-	[estimated_diameter_kilometers_min] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_kilometers_max] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_meters_min] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_meters_max] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_miles_min] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_miles_max] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_feet_min] DECIMAL(8,8) NOT NULL,
-	[estimated_diameter_feet_max] DECIMAL(8,8) NOT NULL,
+	[estimated_diameter_kilometers_min] DECIMAL(8,8) NULL,
+	[estimated_diameter_kilometers_max] DECIMAL(8,8) NULL,
+	[estimated_diameter_meters_min] DECIMAL(8,8) NULL,
+	[estimated_diameter_meters_max] DECIMAL(8,8) NULL,
+	[estimated_diameter_miles_min] DECIMAL(8,8) NULL,
+	[estimated_diameter_miles_max] DECIMAL(8,8) NULL,
+	[estimated_diameter_feet_min] DECIMAL(8,8) NULL,
+	[estimated_diameter_feet_max] DECIMAL(8,8) NULL,
 	[is_potentially_hazardous_asteroid] BOOLEAN NOT NULL,
 	[close_approach_data] DATE NOT NULL,
 	[close_approach_date_full] TIMESTAMPTZ NOT NULL,
@@ -25,5 +25,6 @@ CREATE TABLE [norman_ruiz_coderhouse].[asteroides](
 	[miss_distance_kilometers] DECIMAL(8,8) NOT NULL,
 	[miss_distance_miles] DECIMAL(8,8) NOT NULL,
 	[orbiting_body] VARCHAR(50) NOT NULL,
-	[is_sentry_object] BOOLEAN NOT NULL
-);
+	[is_sentry_object] BOOLEAN NOT null
+)
+SORTKEY(name);
